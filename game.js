1989eeae682e864c2d58b6af456f0a794fbec53d
Segolen <splihon@ucsc.edu>
2023-05-11 18:49:07 -0700

    pushing previous command

--- game.js
+++ game.js
@@ -1,3 +1,19 @@
+class Cover extends Phaser.Scene {
+    constuctor(){
+        super("Cover");
+    }
+    preload(){
+        this.preload.path = './assets/';
+        this.load.image();
+    }
+    create(){
+//tint for behind Play button (to be also used for other buttons)
+//bounce for header??
+//delay on play button??
+    this.input.on('pointer')
+    }
+}
+
 const game = new Phaser.Game({
     scale: {
         mode: Phaser.Scale.FIT,
@@ -5,6 +21,6 @@ const game = new Phaser.Game({
         width: 1920,
         height: 1080
     },
-    scene: [Cover, Level1, Results1, Level2, Results2, Level3, Results3, Level4, Results4, GameOver, GameComplete],
+    scene: [Cover, Level1, Results1, Level2, Results2, Level3, Results3, GameOver, GameComplete],
     title: "Pysics Game",
 });
